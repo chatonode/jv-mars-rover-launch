904a007ede82f20db071e52a90b0b1273e446429
Cevdet Çağdaş Köse <chatonode@gmail.com>
2024-08-15 11:11:19 +0100

    feat: add data classes

---
+++ src/main/java/plateau/XCoords.java
@@ -0,0 +1,19 @@
+package plateau;
+
+public class XCoords {
+    private final int start;
+    private final int end;
+
+    protected XCoords(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
